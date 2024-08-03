58719ee1ee07f49b06c242d51e8f9b336ca85249
Connor Burnside <163656947+mandeIa@users.noreply.github.com>
2024-08-03 02:44:40 -0500

    Update Yeno_Loader.lua

--- Yeno_Loader.lua
+++ Yeno_Loader.lua
@@ -1,4 +1,4 @@
 if not game.IsLoaded(game) then  game.Loaded.Wait(game.Loaded); end
 script_key = getgenv().Yeno.Settings.Whitelist_Key
 
-loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/9cc0f65e4ed67f79684a012a9231253b.lua"))()
+loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/563f01b8434bf25300b22a05ca152422.lua"))()
